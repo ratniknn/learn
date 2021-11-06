9462d57914012a270b575373e09b9ef4dd276a21
Aerocool <antonaumov@gmail.com>
2021-11-06 13:01:42 +0300

    One room

--- 01_inhabitants.py
+++ 01_inhabitants.py
@@ -3,6 +3,9 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+from house1 import room1, room2
 
+
+print('В комнате room_1 живут:', room1.folks)
+print('В комнате room_2 живут:', room2.folks)
 
