4d8bbbfab67a9d56f047caf7d7a97a58b1fa1987
Aerocool <antonaumov@gmail.com>
2021-11-06 14:23:22 +0300

    Rooms

--- 01_inhabitants.py
+++ 01_inhabitants.py
@@ -3,9 +3,11 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-from house1 import room1, room2
+import room_1
+import room_2 as rm2
 
-
-print('В комнате room_1 живут:', room1.folks)
-print('В комнате room_2 живут:', room2.folks)
+str1 = ','.join(room_1.folks)
+str2 = ','.join(rm2.folks)
+print('В комнате room_1 живут:', str1)
+print('В комнате room_2 живут:', str2)
 
